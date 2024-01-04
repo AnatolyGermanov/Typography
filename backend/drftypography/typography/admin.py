aa9f7314a49b50e77164f1815c89cae90e7dcbb3
AnatolyGermanov <tolik.germanov11092002@gmail.com>
2024-01-04 21:22:47 +0300

    add models, change admin panel

--- backend/drftypography/typography/admin.py
+++ backend/drftypography/typography/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import *
+
 
 # Register your models here.
+@admin.register(Staff)
+class StaffAdmin(UserAdmin):
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('patronymic', 'address', 'phone_number',)}),
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('patronymic', 'address', 'phone_number', 'is_banned',)}),
+    )
+
+    list_display = ('username', 'first_name', 'last_name', 'patronymic', 'phone_number', 'is_staff', 'is_banned')
+    search_fields = ('username', 'first_name', 'last_name', 'patronymic', 'email', 'phone_number')
+    list_filter = ('is_banned',)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'patronymic', 'phone_number', 'passport_details', )
+    search_fields = ('first_name', 'last_name', 'patronymic')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'staff', 'receipt_date', 'target_date')
+    search_fields = ('client', )
+    list_filter = ('staff',)
+
+
+@admin.register(OrderDetail)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ('order', 'service', 'amount', )
+    search_fields = ('order', )
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    search_fields = ('title', )
+
+
+@admin.register(ServiceCost)
+class ServiceCostAdmin(admin.ModelAdmin):
+    list_display = ('service', 'cost', )
+    search_fields = ('service', )
+
--- backend/drftypography/typography/models.py
+++ backend/drftypography/typography/models.py
@@ -4,3 +4,53 @@ from django.db import models
 
 # Create your models here.
 class Staff(AbstractUser):
+    patronymic = models.CharField(max_length=150, null=True, blank=True)
+    address = models.CharField(max_length=150, null=True, blank=True)
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
+    is_banned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50, null=True, blank=True)
+    phone_number = models.CharField(max_length=12)
+    passport_details = models.CharField(max_length=10, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Order(models.Model):
+    client = models.ForeignKey('Client', on_delete=models.CASCADE)
+    staff = models.ForeignKey('Staff', on_delete=models.CASCADE)
+    receipt_date = models.DateField(auto_now_add=True)
+    target_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
+
+
+class Service(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class ServiceCost(models.Model):
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    cost = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.service} {self.cost}'
+
